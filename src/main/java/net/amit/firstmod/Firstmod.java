2ccdd52ababd84f567f89bead0605bf8b78354ad
amitvendorog <amitvendorog@gmail.com>
2026-07-05 01:41:23 +0300

    first commit

---
+++ src/main/java/net/amit/firstmod/Firstmod.java
@@ -0,0 +1,18 @@
+package net.amit.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.util.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Firstmod implements ModInitializer {
+	public static final String MOD_ID = "firstmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/amit/firstmod/FirstmodClient.java
@@ -0,0 +1,11 @@
+package net.amit.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstmodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
